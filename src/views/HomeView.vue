7db2873384c7f80b450119ec3763ce2ebc03f554
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-12-30 11:59:14 +0100

    feat: add header with my informations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-  <main>
-
-  </main>
+    <header class="w-full flex justify-center mt-20">
+        <section class="max-w-[500px] flex flex-col items-center justify-center">
+            <img src="../assets/me.webp" alt="Moi" class="w-full max-w-[10%] sm:!max-w-[20%] rounded-full">
+            <h1 class="mt-2 text-gray-200 text-3xl font-semibold">Mathieu Le Puil</h1>
+            <h2 class="mt-2 text-gray-400 text-ml">Développeur Symfony | VueJS | Tailwind</h2>
+        </section>
+  </header>
 </template>
